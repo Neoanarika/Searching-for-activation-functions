e578392c260bfe55c754bf5e2c10296f6e8cdf3a
Ang Ming Liang <angmingliang4017ic@gmail.com>
2018-01-02 22:03:21 +0800

    Updated

--- src/resnet_model.py
+++ src/resnet_model.py
@@ -46,7 +46,7 @@ def batch_norm_relu(inputs, is_training, data_format):
       momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
       scale=True, training=is_training, fused=True)
 
-  unary = {1:lambda x:x ,2:lambda x: -x, 3: lambda x: tf.maximum(x,0.0), 4:lambda x : tf.pow(x,2),5:tf.tanh}
+  unary = {1:lambda x:x ,2:lambda x: -x, 3: lambda x: tf.maximum(x,0), 4:lambda x : tf.pow(x,2),5:tf.tanh}
   binary = {1:lambda x,y: x+y,2:lambda x,y:x*y,3:lambda x,y:x-y,4:lambda x,y:tf.maximum(x,y),5:lambda x,y: tf.sigmoid(x)*y}
   input_fun = {1:lambda x:x , 2:lambda x:0, 3: lambda x:2,4: lambda x : 1, 5: lambda x: -1}
 
