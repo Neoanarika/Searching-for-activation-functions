412d7c40c211a8b2fd6cecbe7affdea074b94e41
Ang Ming Liang <angmingliang4017ic@gmail.com>
2018-01-27 09:36:32 +0800

    updated rnn

--- src/rnn_controller.py
+++ src/rnn_controller.py
@@ -81,7 +81,8 @@ class Network(object):
         return loss
 
     def entropyloss(self,prob):
-        entropy = -tf.reduce_sum(tf.add(tf.exp(tf.log(prob)),tf.log(tf.log(tf.clip_by_value(prob, 1e-10, 1.0)))), axis=1)
+        tf.assert_rank_at_least(tf.log(tf.log(tf.clip_by_value(prob, 1e-10, 1.0))),1,message="clipping is computed wrongly, wrong rank") 
+        entropy = -tf.reduce_sum(tf.exp(tf.add(tf.log(prob),tf.log(tf.log(tf.clip_by_value(prob, 1e-10, 1.0))))), axis=1)
         entropyloss = tf.reduce_mean(entropy, axis=0)  # mean of entropy of pi(obs)
         return entropyloss
 
